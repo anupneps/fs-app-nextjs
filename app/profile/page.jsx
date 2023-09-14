896702a3a9a2300cfdcfbe5402195af25e71fd74
anupneps <anup.neps@outlook.com>
2023-09-14 23:56:57 +0300

    tested frontend CRUD with Api-its working

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,36 +3,48 @@
 import React from 'react'
 import { useSession } from 'next-auth/react'
 import { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import Profile from '@components/profile'
 
 const MyProfile = () => {
     const { data: session } = useSession()
-    const [myPosts, setMyPosts] = useState([])
+    const [posts, setposts] = useState([])
     const router = useRouter()
     
     useEffect(()=>{
         const fetchPosts = async () => {
             const response = await fetch(`/api/users/${session?.user.id}/posts`)
             const data = await response.json()
-            setMyPosts(data)
+            setposts(data)
         }
         if(session?.user.id){
             fetchPosts()
         } 
     }, [session?.user.id])
     
-    const handleEdit = ()=> {
+    const handleEdit = (post)=> {
         router.push(`/update-prompt?id=${post._id}`)
     }
 
-    const handleDelete = async (id) => {}
+    const handleDelete = async (post) => {
+      const hasConfirmed = confirm('Are you sure you want to delete this post?')
+      if(hasConfirmed){
+        try {
+          await fetch(`/api/prompt/${post._id.toString()}`, {method: 'DELETE'})
 
+          const filteredPost = posts.filter((p) => p._id !== post._id)
+          setposts(filteredPost)
+
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
   return (
     <Profile
         name = {`${session?.user.name.split(' ')[0]}'s`}
         desc = "My Profile"
-        data = {myPosts}
+        data = {posts}
         handleEdit = {handleEdit}
         handleDelete = {handleDelete}
     />
